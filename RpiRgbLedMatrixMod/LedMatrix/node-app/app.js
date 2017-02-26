82c7f0dae2aed2f8553876881fb0bbd19dbe6a48
Georg Sandholm <georg.sandholm@xlent.se>
2017-02-26 14:20:35 +0100

    - Added simple nodjs app to send UDP commands

---
+++ RpiRgbLedMatrixMod/LedMatrix/node-app/app.js
@@ -0,0 +1,20 @@
+﻿var PORT = 8888;
+var HOST = '127.0.0.1';
+
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
+var sleep = require('system-sleep');
+
+for (var i = -1; i < 1024; i++) {
+    var message = new Buffer(i + '\n');
+
+    client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+    });
+
+    sleep(100); // sleep for 100ms
+}
+
+client.close();
+
+
